7bfca45b86dd176c348bea115eb8a4093dab1cd2
darshdee <darshilx@hotmail.com>
2026-06-05 23:28:34 -0400

    resume

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,13 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "./Darshil_Desai_Resume_2026_v3.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const pdf = `${process.env.PUBLIC_URL}/resume.pdf`;
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
